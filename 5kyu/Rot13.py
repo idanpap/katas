4dcdf0ad79a650d9d0321b6ce80d6121859df032
Daniel Papastratidis <idanpap@gmail.com>
2021-01-31 19:22:36 +0000

    rot13

---
+++ 5kyu/Rot13.py
@@ -0,0 +1,24 @@
+def rot13(message):
+    arr = [char for char in message]
+    new_array = []
+    for char in arr:
+        if (ord(char) >= 65 and ord(char) <= 90) or (ord(char) >= 97 and ord(char) <= 122):
+            if char.upper() == char:
+                if ord(char)+13 > 90:
+                    new_array.append(chr(ord(char)-13))
+                else:
+                    new_array.append(chr(ord(char)+13))
+            else:
+                if ord(char)+13 > 122:
+                    new_array.append(chr(ord(char)-13))
+                else:
+                    new_array.append(chr(ord(char)+13))
+        else:
+            new_array.append(char)
+
+    return "".join(new_array)
+
+
+print(rot13("Test"))
+#  "grfg")
+# https://www.codewars.com/kata/530e15517bc88ac656000716/train/python

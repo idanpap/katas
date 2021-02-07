10d815a8f604dd2b73a36a551ea1c5c301e92b06
Daniel Papastratidis <idanpap@gmail.com>
2021-02-07 23:00:56 +0000

    Thinking and Testing: A*B?

---
+++ 6-kyu/thinkingAndTestingAB.js
@@ -0,0 +1,15 @@
+function testIt(a, b) {
+  let sumA = 0;
+  let sumB = 0;
+  for (number of a.toString()) {
+    sumA += parseInt(number);
+  }
+  for (number of b.toString()) {
+    sumB += parseInt(number);
+  }
+  return sumA * sumB;
+}
+
+testIt(12, 34);
+//  21
+// https://www.codewars.com/kata/5a90c9ecb171012b47000077/train/javascript
